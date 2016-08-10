0f922b7fbaac91c13b59151ba2020454f5f328eb
Michael Alexander <beefsack@gmail.com>
2016-08-10 13:51:23 +1000

    Fixed args

--- src/tkn.rs
+++ src/tkn.rs
@@ -10,19 +10,23 @@ pub enum Token<'a> {
 }
 
 named!(pub tag_word<&str>, map_res!(take_until_either!(" }"), from_utf8));
-named!(pub tag_arg<&str>,
-       chain!(
-           opt!(multispace) ~
-               arg: alt!(delimited!(char!('"'), map_res!(take_until!("\""), from_utf8), char!('"')) | tag_word) ,
-           || { arg }
-       ));
+named!(pub tag_arg<&str>, alt!(
+    delimited!(char!('"'), map_res!(take_until!("\""), from_utf8), char!('"')) |
+    tag_word
+));
+named!(pub tag_args<Vec<&str> >, separated_list!(multispace, tag_arg));
 named!(pub tag<Token>, delimited!(tag!("{{"), chain!(
     pre: opt!(one_of!("#/")) ~
         opt!(multispace) ~
         tag_name: tag_word ~
-        args: many0!(tag_arg) ~
+        opt!(multispace) ~
+        opt_args: opt!(tag_args) ~
         opt!(multispace) ,
     || {
+        let args = match opt_args.clone() {
+            Some(a) => a,
+            None => vec![],
+        };
         match pre {
             Some('#') => Token::TagBlockOpen(tag_name, args),
             Some('/') => Token::TagBlockClose(tag_name),
@@ -41,6 +45,11 @@ mod tests {
         assert_eq!(tag_arg(b"cheese}"), Done(&b"}"[..], "cheese"));
         assert_eq!(tag_arg(b"cheese "), Done(&b" "[..], "cheese"));
         assert_eq!(tag_arg(b"\"cheese bacon tomato\""), Done(&b""[..], "cheese bacon tomato"));
+    }
+
+    #[test]
+    fn tag_args_works() {
+        assert_eq!(tag_args(b"cheese bacon tomato}}"), Done(&b"}}"[..], vec!["cheese", "bacon", "tomato"]));
     }
 
     #[test]
