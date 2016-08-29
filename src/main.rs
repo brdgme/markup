701153a6516790f25825cf32ed78ed38638e6fea
Michael Alexander <beefsack@gmail.com>
2016-08-29 23:00:28 +1000

    Changed test script to use nodes instead of parsing

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,15 @@
 extern crate brdgme_markup;
 
-use brdgme_markup::{ansi, parse};
+use brdgme_markup::ansi;
+use brdgme_markup::ast::{Node, Cell, Align};
 
 fn main() {
     println!("{}",
-             ansi(&parse(r#"
-{{#b}}Egg{{/b}}
-{{#table}}
-  {{#row}}
-    {{#cell}}Boo{{/cell}}
-    {{#cell}}Bam{{/cell}}
-  {{/row}}
-{{/table}}
-"#)
-                      .unwrap(),
+             ansi(&vec![
+                 Node::Table(vec![
+                     vec![Cell{ align: Align::Left, children: vec![Node::Text("moo".to_string())] }],
+                 ]),
+             ],
                   &vec!["mick".to_string(), "steve".to_string()])
                  .unwrap());
 }
